199a5d0d43da7a0724d789654d2f143a533a498c
Reda Benyoub <reda.benyoub@e-adsi.com>
2021-02-11 09:59:01 +0100

    mise à jour

--- djangoDash/settings.py
+++ djangoDash/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'u2wkpk7#_2k97^#^+ds5fhv+_ibyjkp3*g%=lo%!*o!svt8q9j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
